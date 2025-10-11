2bd06d49620730efb413530b3a8493a03ec08cca
Jenna (HopeTS) <jennabaudelaire@pm.me>
2025-10-11 05:30:46 -0400

    force a trigger of a dynamic save

--- saving-peardrive/index.js
+++ saving-peardrive/index.js
@@ -35,10 +35,6 @@ await peer1.ready();
 await peer1.joinNetwork();
 console.log("Peer 1 is ready");
 
-// Save the network key for later
-const ORIGINAL_PUBLIC_KEY = peer1.publicKey;
-const ORIGINAL_NETWORK_KEY = peer1.networkKey;
-
 /*
  * Here are the two methods of saving a PearDrive instance.
  */
@@ -64,54 +60,83 @@ console.log("Peer 1 saved to:", SAVE_1_PATH);
 // To do this, we will tap into the SAVE_DATA_UPDATE event, which will pass the
 // updated save data whenever it changes. We will then write that data to a file
 // each time it changes.
-//
-// Keep in mind that in this use case, there won't be any save data saved
-// because this event only runs whenever the save data changes, and we aren't
-// doing anything that would change it in this example. But if you were to do
-// downloads, the save data would change and be saved to the file.
 peer1.on(EVENT.SAVE_DATA_UPDATE, (newSaveData) => {
   fs.writeFileSync(SAVE_2_PATH, JSON.stringify(newSaveData, null, 2));
   console.log("Peer 1 save data updated:", SAVE_2_PATH);
 });
 
+// Now there is no initial save event that fires when you add the listener, but
+// whenever anything happens that changes the save data, the listener will fire.
+// For example, if we change a setting, like activating relay mode, the save
+// data will update and the listener will fire.
+console.log("Activating relay mode on Peer 1...");
+peer1.activateRelay();
+
+// Wait a few seconds to ensure the save data has time to update
+await new Promise((resolve) => setTimeout(resolve, 2000));
+
+// Save the relavant initial save data for comparison later
+const ORIGINAL_PUBLIC_KEY = peer1.publicKey;
+const ORIGINAL_NETWORK_KEY = peer1.networkKey;
+const ORIGINAL_RELAY = peer1.relay;
+
 /*
- * Now we are going to close out peer1, and then reload it. This process is the
- * same for both methods, since they both create a save file that can be read
- * back in. There also won't be any difference between the two save files in
- * this example, since we aren't doing any downloads that would change the save
- * data.
+ * Now we are going to close out peer1, and then reload it once with the static
+ * save method, and once with the dynamic save method. As you will see, live
+ * changes made to the peer after initialization (like activating relay) are
+ * not saved with the static method, but they are saved with the dynamic method.
  */
 
 // Close out peer1
 await peer1.close();
 console.log("Peer 1 closed.");
 
-// Now let's grab the save data from the first method
-const savedData1 = JSON.parse(fs.readFileSync(SAVE_1_PATH));
+// Now let's grab the save data from the static method first
+const staticSaveData = JSON.parse(fs.readFileSync(SAVE_1_PATH));
 
 // To reload a PearDrive instance from save data, you just pass the saved
 // data to the PearDrive constructor, and then ready() and joinNetwork() as
 // normal. You should always joinNetwork without an argument when reloading,
 // since the network key is already stored in the save data.
-const reloadedPeer1Static = new PearDrive(savedData1);
-await reloadedPeer1Static.ready();
-await reloadedPeer1Static.joinNetwork();
+const staticSavePeer1 = new PearDrive(staticSaveData);
+await staticSavePeer1.ready();
+await staticSavePeer1.joinNetwork();
 console.log("");
 console.log("Peer 1 reloaded from static save data is ready.");
 console.log("Original public key:", ORIGINAL_PUBLIC_KEY);
-console.log("Reloaded public key:", reloadedPeer1Static.publicKey);
+console.log("Static save peer1 public key:", staticSavePeer1.publicKey);
 console.log("---");
 console.log("Original network key:", ORIGINAL_NETWORK_KEY);
-console.log("Reloaded network key:", reloadedPeer1Static.networkKey);
+console.log("Static save peer1 network key:", staticSavePeer1.networkKey);
 console.log(
-  "^ These two sets of keys match because this is the same peer instance \
-  reloaded."
+  "^ (These two sets of keys match because this is the same peer instance \
+  reloaded.)"
 );
-console.log("");
+// As you can see here though, because we used the static save method, the relay
+// mode is not activated, even though we activated it before closing the peer.
+// This is because by default, relay is deactivated.
+console.log("---");
+console.log("Original relay mode:", ORIGINAL_RELAY);
+console.log("Static save peer1 relay mode:", staticSavePeer1.relay);
 
 // It's that simple! So now we close the peer.
-await reloadedPeer1Static.close();
-console.log("Peer 1 reloaded from static save data closed.");
+await staticSavePeer1.close();
+console.log("Static save peer1 closed.");
+
+// Now let's grab the save data from the dynamic method
+const dynamicSaveData = JSON.parse(fs.readFileSync(SAVE_2_PATH));
+
+// Now we reload the PearDrive instance from the dynamic save data
+const dynamicSavePeer1 = new PearDrive(dynamicSaveData);
+await dynamicSavePeer1.ready();
+await dynamicSavePeer1.joinNetwork();
+console.log("");
+console.log("Peer 1 reloaded from dynamic save data is ready.");
+console.log("Original public key:", ORIGINAL_PUBLIC_KEY);
+console.log("Dynamic save peer1 public key:", dynamicSavePeer1.publicKey);
+console.log("---");
+console.log("Original network key:", ORIGINAL_NETWORK_KEY);
+console.log("Dynamic save peer1 network key:", dynamicSavePeer1.networkKey);
 
 ////////////////////////////////////////////////////////////////////////////////
 // Helper functions
